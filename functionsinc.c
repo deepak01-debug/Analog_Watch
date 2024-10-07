c99a9b53c316c775d7bcfba93d7a413426ad9809
deepak <e4561994@gmail.com>
2024-10-07 09:04:48 +0530

    first commit

---
+++ functionsinc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+    void fun1(int a){
+        printf("%d\n",a);
+    }
+
+void myfunc(){
+    printf("hello Deepak!");
+}
+
+int main(){
+fun1(2);
+myfunc();
+return 0;
+}
---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int main(){
+
+// int day;
+// printf("Enter your day");
+// scanf("%d",&day);
+
+// switch (day)
+// {
+// case 1:
+//     printf("monday");
+//     break;
+//     case 2:
+//     printf("tueday");
+//     break;
+//     case 3:
+//     printf("wednesday");
+//     break;
+//     case 4:
+//     printf("thursday");
+//     break;
+//     case 5:
+//     printf("friday");
+//     break;
+//     case 6:
+//     printf("saturday");
+//     break;
+//     case 7:
+//     printf("sunday");
+//     break;
+// default:
+//     break;
+// }
+
+// int n;
+
+// printf("Enter Your nu,ber=>");
+// scanf("%d",&n);
+
+// if(n>0){
+//     printf("Your number is a Positive Number");
+// }
+
+// else if (n<0)
+// {
+//     printf("Your number is a Negative number ");
+// }
+
+// else{
+//     printf("your number is equal to zero");
+// }
+
+ int isProgrammingFun = 111;
+int  isFishTasty = 123;
+
+// Return boolean values
+printf("%d", isProgrammingFun);   // Returns 1 (true)
+printf("%d", isFishTasty);        // Returns 0 (false)
+
+    return 0;
+}
+
+
